c76d03bc4391d4f69fd65e9822f2dd236948d25a
Muhammad Khalid Kamran <60957789+kkamran245@users.noreply.github.com>
2020-03-17 06:39:04 +0000

    PHP Learning
    PHP Classes Update

---
+++ PHP/1.php
@@ -0,0 +1,46 @@
+<?php
+
+$num = 1;
+echo "</br>";
+echo $num;
+
+$name = "Khalid";
+echo "</br>";
+echo "My Name is $name";
+
+$float_number = 1.1;
+echo "</br>";
+echo $float_number;
+
+$dotNum = 2.33454545454;
+echo "</br>";
+echo "$dotNum";
+
+$One = true;
+$Zero = false;
+echo $One;
+
+$array_name = array("Muhammad","Khalid",'Kamran',4,true);
+echo "</br>";
+echo $array_name[4];
+
+$num1 = 10;
+$num2 = 50;
+
+$sum = $num1 + $num2;
+echo "</br>";
+echo "The sum of two numbers is = $sum";
+
+$sum1 = $num2 - $num1;
+echo "</br>";
+echo "If we minus 10 from 50, the result will be = $sum1";
+
+$mult = $num2 * $num1;
+echo "</br>";
+echo "If we multiply 10 with 50, the result will be = $mult";
+
+$divi = $num2 / $num1;
+echo "</br>";
+echo "If we multiply 10 with 50, the result will be = $divi";
+
+?>
